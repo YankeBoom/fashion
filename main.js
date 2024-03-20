10a9f12d7aa101dc21d691b097a01392fd4e1d75
YankeBoom <118504782+YankeBoom@users.noreply.github.com>
2024-03-20 11:40:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.swiper', {
+    cssMode: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+  });
